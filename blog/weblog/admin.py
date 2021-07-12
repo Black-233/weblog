cbe1c2eda717b42f8a5555f6c11996d033e78b50
周日昇 <2237568074@qq.com>
2021-07-12 14:25:29 +0800

    ok

--- blog/weblog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Entry, Topic, Tag
-# Register your models here.
-
-admin.site.register(Entry)
-admin.site.register(Topic)
-admin.site.register(Tag)
--- blog/weblog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Entry, Topic, Tag
-import datetime
-# Create your views here.
-def index(request):
-	entrys = Entry.objects.all()
-	context = {"now_year": "{}".format(datetime.date.today().year), "entrys_list": entrys}
-	return render(request, 'weblog/index.html', context)
